ceae8b42b502e88834bf5bdb063538f7acd50f1e
wygod <2667946747@qq.com>
2024-01-31 17:38:20 +0800

    add celert

---
+++ celery_work.py
@@ -0,0 +1,47 @@
+# -*-encoding:utf-8 -*-
+
+from celery import Celery
+
+import uiautomator2.exceptions
+from collection.we_collection import *
+from common.collectionCommon import InitVenv
+
+broker = 'redis://localhost:6379/1'
+backend = 'redis://localhost:6379/2'
+
+app = Celery(
+    'celeryDemo',
+    broker=broker,
+    backend=backend
+)
+
+
+def spider_run(parse_config_value, device_ip):
+    we_run_app = WeCollectionOperator(parse_config_value, device_ip)
+    try:
+        we_run_app.destroy_current_app()
+        time.sleep(2)
+        we_run_app.start_current_app()
+        we_run_app.move_to_button()
+        we_run_app.cycle_living_store()
+        we_run_app.destroy_current_app()
+    except uiautomator2.exceptions.GatewayError as e:
+        we_run_app.start_uiautomator2()
+        we_run_app.check_spider_status()
+        print(e)
+
+
+@app.task()
+def main(device):
+    parse_config_value = WeConfigParse.parse_base_config('config/config.yaml')
+    iter_number = 0
+    while True:
+        try:
+            spider_run(parse_config_value, device)
+        except uiautomator2.exceptions.UiAutomationNotConnectedError as e:
+            init_env = InitVenv(config=parse_config_value, device_serial=device)
+            init_env.check_atx_instrument(low=8 if iter_number < 8 else iter_number)
+            if init_env.ui_device.uiautomator.running():
+                iter_number = 0
+                print('restart atx successful')
+            print(e)
